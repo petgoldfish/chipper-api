cfa55669314e7dffbe4b502339b99894b46d178e
Raghav Sai <g.raghav.sai@gmail.com>
2020-05-12 16:32:00 -0400

    Added Me Query

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -7,9 +7,12 @@ import {
 	Arg,
 	Query,
 	Int,
+	Ctx,
 } from "type-graphql";
 import { User } from "../entities/User";
 import { Chirp } from "../entities/Chirp";
+import { authAndGetUserId } from "../utils";
+import { Context } from "../types/Context";
 
 @Resolver((of) => User)
 export class UserResolver implements ResolverInterface<User> {
@@ -19,8 +22,20 @@ export class UserResolver implements ResolverInterface<User> {
 	}
 
 	@Query((returns) => [User])
-	async users() {
+	async users(@Ctx() context: Context) {
+		authAndGetUserId(context);
 		return User.find({
+			relations: ["chirps"],
+		});
+	}
+
+	@Query((returns) => User)
+	async me(@Ctx() context: Context) {
+		const userId = authAndGetUserId(context);
+		return User.findOne({
+			where: {
+				id: userId,
+			},
 			relations: ["chirps"],
 		});
 	}
